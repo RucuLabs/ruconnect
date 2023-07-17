fbd11101b964dbcd36a46a6bbdcddbea48e82c75
iPolarisu <elpolarisu@gmail.com>
2023-07-17 18:28:57 -0400

    feat: handle raw email

--- email.py
+++ email.py
@@ -1,37 +1,31 @@
 import imaplib
 import time
 
-IMAP_HOST = 'imap.ex.com'
 IMAP_PORT = 993
-USERNAME = 'user@ex.com'
-PASSWORD = 'pass'
 
-TIME = 10
+def check_emails(user, password, imap_host):
+    try:
+        imap = imaplib.IMAP4_SSL(imap_host, IMAP_PORT)
+        imap.login(user, password)
+        imap.select('INBOX')
+        status, response = imap.search(None, '(UNSEEN)')
+        message_ids = response[0].split()
 
-def print_email_content(email_data):
-    print(email_data)
-
-def check_emails():
-    while True:
-        try:
-            imap = imaplib.IMAP4_SSL(IMAP_HOST, IMAP_PORT)
-            imap.login(USERNAME, PASSWORD)
-            imap.select('INBOX')
-            status, response = imap.search(None, '(UNSEEN)')
-            message_ids = response[0].split()
-
-            for message_id in message_ids:
-                status, response = imap.fetch(message_id, '(RFC822)')
-                raw_email = response[0][1]
-                print_email_content(raw_email)
-                imap.store(message_id, '+FLAGS', '\\Seen')
-
+        if len(message_ids) == 0:
             imap.logout()
+            return []
 
-        except Exception as e:
-            print('Error:', str(e))
+        new_mail = []
 
-        time.sleep(TIME)
+        for message_id in message_ids:
+            status, response = imap.fetch(message_id, '(RFC822)')
+            raw_email = response[0][1]
+            imap.store(message_id, '+FLAGS', '\\Seen')
+            new_mail.append(raw_email)
 
-check_emails()
+        imap.logout()
+        return new_mail
 
+    except Exception as e:
+        print('Error:', str(e))
+        return []
